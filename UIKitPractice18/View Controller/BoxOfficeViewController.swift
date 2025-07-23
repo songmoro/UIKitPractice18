6f81eb8293e7d6f8e2af17ac0ec2505a20ef62aa
songmoro <wlqgkr@naver.com>
2025-07-23 19:21:11 +0900

    로또 로직 개선 | 박스오피스 디자인

--- UIKitPractice18/View Controller/BoxOfficeViewController.swift
+++ UIKitPractice18/View Controller/BoxOfficeViewController.swift
@@ -6,11 +6,200 @@
 //
 
 import UIKit
+import SnapKit
+
+protocol IsIdentifiable {
+    static var identifier: String { get }
+}
+
+extension IsIdentifiable {
+    static var identifier: String {
+        String(describing: Self.self)
+    }
+}
+
+extension UITableView {
+    func register(_ cellClass: (AnyObject & IsIdentifiable).Type) {
+        self.register(cellClass, forCellReuseIdentifier: cellClass.identifier)
+    }
+}
+
+extension UITableView {
+    func dequeueReusableCell(_ cellClass: (AnyObject & IsIdentifiable).Type, for indexPath: IndexPath) -> UITableViewCell {
+        self.dequeueReusableCell(withIdentifier: cellClass.identifier, for: indexPath)
+    }
+}
+
+class RatingView: UIView {
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "888"
+        label.textColor = .systemBackground
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        addSubview(ratingLabel)
+        backgroundColor = .label
+        
+        ratingLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+}
+
+class MovieCell: UITableViewCell, IsIdentifiable {
+    let ratingView = RatingView()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "엽문 4: 더 파이널"
+        
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "8888-88-88"
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        addSubviews(ratingView, titleLabel, dateLabel)
+        
+        ratingView.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.8)
+            $0.width.equalToSuperview().multipliedBy(0.15)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(ratingView.snp.trailing).offset(12)
+            $0.trailing.equalTo(dateLabel).inset(12)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+    }
+}
 
 class BoxOfficeViewController: UIViewController {
+    let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.placeholder = "20200401"
+        
+        return textField
+    }()
+    
+    // TODO: 보관/폐기 고민
+    let searchTextFieldUnderline: UIView = {
+        let view = UIView()
+        view.backgroundColor = .label
+        
+        return view
+    }()
+    
+    let searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("검색", for: .normal)
+        button.setTitleColor(.systemBackground, for: .normal)
+        button.backgroundColor = .label
+        
+        return button
+    }()
+    
+    let movies = MovieInfo.movies
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.keyboardDismissMode = .interactive
+        tableView.separatorStyle = .none
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        configureTableView()
+    }
+}
+
+extension BoxOfficeViewController {
+    func configure() {
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .systemTeal
+        view.addSubviews(searchTextField, searchTextFieldUnderline, searchButton, tableView)
+        
+        searchTextField.snp.makeConstraints {
+            $0.top.equalToSuperview(\.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalToSuperview().offset(16)
+            $0.width.equalToSuperview().multipliedBy(0.65)
+            $0.height.equalTo(48)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.leading.equalTo(searchTextField.snp.trailing).offset(12)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.centerY.equalTo(searchTextField)
+        }
+        
+        searchTextFieldUnderline.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalTo(searchTextField)
+            $0.height.equalTo(4)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.leading.equalTo(searchTextField)
+            $0.trailing.equalTo(searchButton)
+            $0.top.equalTo(searchTextField.snp.bottom).offset(12)
+            $0.bottom.equalToSuperview(\.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension BoxOfficeViewController: UITableViewDelegate, UITableViewDataSource {
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.layer.borderColor = UIColor.blue.cgColor
+        tableView.layer.borderWidth = 2
+        tableView.register(MovieCell.self)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(MovieCell.self, for: indexPath)
+        
+        
+        return cell
     }
 }
---
+++ UIKitPractice18/View Controller/Lotto/CircleView.swift
@@ -0,0 +1,64 @@
+//
+//  CircleView.swift
+//  UIKitPractice18
+//
+//  Created by 송재훈 on 7/23/25.
+//
+
+import UIKit
+import SnapKit
+
+class CircleView: UIView {
+    let numberLabel = UILabel()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(width: CGFloat, number: Int) {
+        self.init(length: width, number: number)
+    }
+    
+    convenience init(height: CGFloat, number: Int) {
+        self.init(length: height, number: number)
+    }
+    
+    convenience init(width: CGFloat) {
+        self.init(length: width)
+    }
+    
+    convenience init(height: CGFloat) {
+        self.init(length: height)
+    }
+    
+    private init(length: CGFloat) {
+        super.init(frame: CGRect(x: 0, y: 0, width: length, height: length))
+        configureLayout(length)
+        
+        numberLabel.text = "+"
+    }
+    
+    private init(length: CGFloat, number: Int) {
+        super.init(frame: CGRect(x: 0, y: 0, width: length, height: length))
+        configureLayout(length)
+        
+        numberLabel.text = number.description
+        numberLabel.textColor = .systemBackground
+    }
+    
+    private func configureLayout(_ length: CGFloat) {
+        addSubview(numberLabel)
+        
+        numberLabel.textAlignment = .center
+        
+        numberLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        snp.makeConstraints {
+            $0.size.equalTo(CGSize(width: length, height: length))
+        }
+        
+        layer.cornerRadius = length / 2
+    }
+}
